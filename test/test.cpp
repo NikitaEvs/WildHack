a1df784ef150efe5ab545c694e80257b7e8ecc2d
ris-iris <sagitova.ma@phystech.edu>
2020-04-05 19:25:02 +0300

    Edit example file

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,6 @@
 #include "Map/CellDirector.h"
 #include "Map/CellBuilder.h"
-
+#include "PopulationDirector.h"
 #include <iostream>
 
 int main() {
@@ -11,6 +11,12 @@ int main() {
   std::shared_ptr<Cell> waterCell = builder->getProduct();
   std::cout << *waterCell << std::endl;
   delete builder;
+
+  PopulationDirector d;
+  std::shared_ptr<PopulationBuilder> b = std::make_shared<HerbivorePopulationBuilder>();
+  d.setBuilder(b);
+  std::shared_ptr<Population> p = d.makeBig("elephant");
+  std::cout << *p;
 
   return 0;
 }
