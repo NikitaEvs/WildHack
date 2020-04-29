c856488677a4eeb148474ab5c455fb3c93c7cc83
NikitaEvs <nikitaevsykov2@gmail.com>
2020-04-29 17:05:31 +0300

    Add Map class with loader and saver

---
+++ src/Map/Map.h
@@ -0,0 +1,30 @@
+#ifndef PEACH_SRC_MAP_MAP_H_
+#define PEACH_SRC_MAP_MAP_H_
+
+#include <memory>
+#include <vector>
+
+#include "CellType.h"
+#include "json/json.hpp"
+
+class Map {
+ private:
+  std::vector<std::vector<std::shared_ptr<CellType> > > map;
+
+ public:
+  typedef std::vector<std::vector<std::shared_ptr<CellType> > >::iterator iterator;
+
+  Map(size_t columnNumber, size_t rowNumber);
+  Map() = default;
+
+  iterator begin();
+  iterator end();
+  std::vector<std::shared_ptr<CellType> >& operator[](size_t index);
+
+  void loadFrom(std::istream& file);
+  void saveTo(std::ostream& out);
+
+  void addCell(std::shared_ptr<CellType> &&cell, size_t row, size_t column);
+};
+
+#endif //PEACH_SRC_MAP_MAP_H_
