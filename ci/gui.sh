6c11ffdf7a6ce02574e836c70c34672e1026f51b
NikitaEvs <nikitaevsykov2@gmail.com>
2020-05-01 15:27:12 +0300

    Fix CI

--- ci/gui.sh
+++ ci/gui.sh
@@ -4,4 +4,10 @@ echo "Setup GUI ^_^"
 
 sudo apt-get install build-essential
 sudo apt-get install qt5-default
-sudo apt-get install qtdeclarative5-dev qml-module-qtquick-controls2
+sudo apt-get install qtdeclarative5-dev
+sudo apt-get install qml-module-qtquick-controls
+sudo apt-get install qml-module-qtquick-controls2
+sudo apt-get install qtdeclarative5-controls-plugin
+sudo apt-get install qtdeclarative5-quicklayouts-plugin
+sudo apt-get install qtdeclarative5-qtquick2-plugin
+sudo apt-get install qtdeclarative5-window-plugin
