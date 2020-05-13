912f5015db94c11fc09fce16951c5b3174c81846
ris-iris <sagitova.ma@phystech.edu>
2020-05-13 09:16:07 +0300

    another fix

--- src/Engine/Player/Player.cpp
+++ src/Engine/Player/Player.cpp
@@ -9,16 +9,17 @@ void Player::addNewPopulation(std::shared_ptr<Population> new_population) {
   playerPopulations.push_back(new_population);
 }
 
-
 const std::vector<std::shared_ptr<Population>> &Player::GetPlayerPopulations() const {
   return playerPopulations;
 }
 void Player::SetPlayerPopulations(const std::vector<std::shared_ptr<Population>> &player_populations) {
   playerPopulations = player_populations;
 }
-const std::__cxx11::basic_string<char> &Player::GetName() const {
+const std::string &Player::GetName() const {
   return name;
 }
-void Player::SetName(const std::__cxx11::basic_string<char> &name) {
+void Player::SetName(const std::string &name) {
   Player::name = name;
 }
+
+
--- src/Engine/Player/Player.h
+++ src/Engine/Player/Player.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <memory>
+#include <string>
 
 #include "PopulationDirector.h"
 
