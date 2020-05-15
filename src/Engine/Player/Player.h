8ac8e812f0bb8f715308618613e77b7db7a29bfb
NikitaEvs <nikitaevsykov2@gmail.com>
2020-05-15 13:58:45 +0300

    Player fix

--- src/Engine/Player/Player.h
+++ src/Engine/Player/Player.h
@@ -8,7 +8,7 @@
 
 class Player {
  public:
-  explicit Player(const std::string& name);
+  explicit Player(int32_t setID);
 
   Player() = default;
 
@@ -18,13 +18,34 @@ class Player {
   const std::vector<std::shared_ptr<Population>> &GetPlayerPopulations() const;
   void SetPlayerPopulations(const std::vector<std::shared_ptr<Population>> &player_populations);
 
+  int32_t GetId() const;
+  void SetId(int32_t id);
+
   const std::string &GetName() const;
   void SetName(const std::string &name);
 
   void generatePopulations();
 
   size_t getPopulationsNumber();
+
+  size_t calculateScore() const;
  private:
   std::vector<std::shared_ptr<Population> > playerPopulations;
+
   std::string name;
+
+  int32_t id;
+};
+
+class LightPlayer {
+ public:
+  LightPlayer(const Player& player);
+
+  const std::string &getName() const;
+  void setName(const std::string &name);
+  size_t getScore() const;
+  void setScore(size_t score);
+ private:
+  std::string name;
+  size_t score = 0;
 };
